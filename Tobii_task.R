afdf9405cb6ede4aa8dbc1c4233fbee89c41906b
Matt Ragoza <mtr22@pitt.edu>
2021-02-10 14:13:00 -0500

    fix stdize_col function to modify colnames, and return modified dataframe, also add comments

--- Tobii_task.R
+++ Tobii_task.R
@@ -1,5 +1,4 @@
 
-
 # Packages
 library(dplyr)
 library(tidyverse)
@@ -7,42 +6,62 @@ library(testit)
 library(rlist)
 library(readxl)
 
-stdize_col <- function(pattern, dataframe, new_col){
+stdize_col <- function(dataframe, pattern, new_col){
+  
+  # get the index of the column that matches pattern
   col_idx <- grep(pattern, colnames(dataframe))
+  
+  # check whether any column index was found
   assert("Column not found that matches pattern", col_idx != 0)
-  return(dataframe[col_idx])
+  
+  # assign new_col name to the column at the found index
+  colnames(dataframe)[col_idx] <- new_col
+  
+  # return the modified dataframe to keep the changes
+  return(dataframe)
 }
 
-# Creating a for lop for 
-example_data_file <- read_xlsx("\\\\psychodrama2.univ.pitt.edu\\Studies\\Girls Brain\\Tobii Glasses\\Raw Data\\T1 Speech Task Reexported for Mary\\GIRLS 22017 T1D2 Child Data Export.xlsx")
+#data_dir <- "\\\\psychodrama2.univ.pitt.edu\\Studies\\Girls Brain\\Tobii Glasses\\Raw Data\\T1 Speech Task Reexported for Mary"
+data_dir <- "C:\\Users\\mtr22\\Code\\FEND-LAb-Tasks\\data"
+
+example_data_file <- read_xlsx(file.path(data_dir, "22206 GIRLS T1D2 Child Data Export.xlsx"))
 ls(example_data_file)
 
-data_dir <- "\\\\psychodrama2.univ.pitt.edu\\Studies\\Girls Brain\\Tobii Glasses\\Raw Data\\T1 Speech Task Reexported for Mary"
+# list the data files in data directory
 data_files <- list.files(data_dir)
+
+# create empty list to accumulate data frames
 n_data_files <- 3
 #n_data_files <- length(data_files)
 dfs <- vector("list", n_data_files)
 
+# for each data file,
 for (i in 1:n_data_files) {
+  
+  # read in the data file
   raw_df <- read_xlsx(file.path(data_dir, data_files[i]))
-  raw_df$subject <- raw_df$`Participant name`
-  raw_df$manual_map_x <- stdize_col("Manually mapped gaze point X", raw_df)
-  raw_df$manual_map_y <- stdize_col("Manually mapped gaze point Y", raw_df)
-  raw_df$gaze_final_x <- stdize_col("Mapped gaze point X", raw_df)
-  raw_df$gaze_final_y <- stdize_col("Mapped gaze point Y", raw_df)
+  
+  # rename the needed columns to standardized names
+  raw_df <- stdize_col(raw_df, "Participant name", "subject")
+  raw_df <- stdize_col(raw_df, "Manually mapped gaze point X", "manual_map_x")
+  raw_df <- stdize_col(raw_df, "Manually mapped gaze point Y", "manual_map_y")
+  raw_df <- stdize_col(raw_df, "Mapped gaze point X", "gaze_final_x")
+  raw_df <- stdize_col(raw_df, "Mapped gaze point Y", "gaze_final_y")
 
+  # select only the standardized columns and put data frame in list
+  dfs[[i]] <- raw_df[, c(
+    "subject", "manual_map_x", "manual_map_y", "gaze_final_x", "gaze_final_y"
+  )]
+  
+  # display the progress
   print(paste(data_files[i], " ", i, "/", n_data_files))
-  dfs[[i]] <- raw_df[, 
-    c("subject", "manual_map_x", "manual_map_y", "gaze_final_x", "gaze_final_y")
-  ]
 }
 
+# combine all the data frames in list
 combined_df <- bind_rows(dfs)
+
 colnames(combined_df)
 unique(combined_df$subject)
-
-combin
-
 group_by(combined_df, subject) %>% count()
 
 group_by(combined_df, subject) %>% summarise_if(is.numeric)
