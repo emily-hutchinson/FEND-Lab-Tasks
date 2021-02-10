65a97ad004e3586fb241a6ba1409280f6bd39309
emily-hutchinson <emh142@pitt.edu>
2021-02-09 20:37:10 -0500

    Fixing for loop to work to add files to list; adding in line to idex columns of interest by position

--- Tobii_task.R
+++ Tobii_task.R
@@ -5,38 +5,22 @@
 tobii_22001 <- read_xlsx("\\\\psychodrama2.univ.pitt.edu\\Studies\\Girls Brain\\Tobii Glasses\\Raw Data\\T1 Speech Task Reexported for Mary")
 qwerty <- tobii_22001
 
-# Creating a for loop
+# Creating a for
 data_dir <- "\\\\psychodrama2.univ.pitt.edu\\Studies\\Girls Brain\\Tobii Glasses\\Raw Data\\T1 Speech Task Reexported for Mary"
 data_files <- list.files(data_dir)
-dfs <- list()
-for (f in data_files) {
-  df <- read_xlsx(file.path(data_dir, f))
-  print(f)
-  list.append(dfs, df)
+n_data_files <- length(data_files)
+dfs <- vector("list", n_data_files)
+for (i in 1:n_data_files) {
+  df <- read_xlsx(file.path(data_dir, data_files[i]))
+  col_idx <- grep("Manually mapped gaze point X", colnames(df))
+  assert("Column not found that matches pattern", col_idx != 0)
+  df$manual_map_x <- df[col_idx]
+  print(data_files[i])
+  dfs[[i]] <- df
 }
 
-# List of column numbers in Tobii datasets
-ls(tobii_22001)
-
-#[33] "Manually mapped gaze point X [22001_childPOV_speech]"    
-#[34] "Manually mapped gaze point Y [22001_childPOV_speech]" 
-#[39] "Mapped gaze point X [22001_childPOV_speech]"             
-#[40] "Mapped gaze point Y [22001_childPOV_speech]"  
-#[41] "Participant name" 
-
-
-# Renaming column - working 
-tobii_22001 <- rename(tobii_22001, 
-                      manual_map_x = `Manually mapped gaze point X [22001_childPOV_speech]`) # Manually mapped X
-tobii_22001 <- rename(tobii_22001, 
-                      manual_map_y = `Manually mapped gaze point Y [22001_childPOV_speech]`) # Manually mapped Y
-tobii_22001 <- rename(tobii_22001, 
-                      gaze_point_map_x = `Mapped gaze point X [22001_childPOV_speech]`) # Mapped gaze point X 
-tobii_22001 <- rename(tobii_22001, 
-                      gaze_point_map_y = `Mapped gaze point Y [22001_childPOV_speech]`) # Mapped gaze point Y
-
 # Indexing x 
-idx <- grep("Manually mapped gaze point X", colnames(tobii_22001))
+idx <- grep("Manually mapped gaze point X", colnames(df))
 assert("Column not found that matches pattern", idx != 0)
 tobii_22001$manual_map_x <- tobii_22001[idx]
 
